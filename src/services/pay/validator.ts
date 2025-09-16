2132d92b4c8478a1f127d55eaa44827ee1cce243
BhavsarShubham <shubhambhavsar1133@gmail.com>
2025-09-16 13:07:32 +0530

    Updated routs

--- src/services/pay/validator.ts
+++ src/services/pay/validator.ts
@@ -2,15 +2,30 @@ import { payResponseSchema, payWithAddressSchema, payWithEmailSchema, payWithPay
 import type { PayResponse, PayWithAddressBody, PayWithEmailBody, PayWithPaymailBody } from './types.js';
 
 export default class Validator {
+    /**
+     * Validate payload for transfer via Address
+     */
     payWithAddress(option: PayWithAddressBody) {
         return payWithAddressSchema.parse(option);
     }
+
+    /**
+     * Validate payload for transfer via Email
+     */
     payWithEmail(option: PayWithEmailBody) {
         return payWithEmailSchema.parse(option);
     }
+
+    /**
+     * Validate payload for transfer via Paymail
+     */
     payWithPaymail(option: PayWithPaymailBody) {
         return payWithPaymailSchema.parse(option);
     }
+
+    /**
+     * Validate transfer response
+     */
     payResponse(option: PayResponse) {
         return payResponseSchema.parse(option);
     }
